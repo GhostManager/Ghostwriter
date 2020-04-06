81335322a04243dd9cff3b1d4a9640e41fd49fae
Christopher Maddalena <chris.maddalena@protonmail.com>
2020-04-05 21:09:39 -0700

    Set X_FRAME_OPTIONS to SAMEORIGIN for URLDialog

--- config/settings/base.py
+++ config/settings/base.py
@@ -208,7 +208,7 @@ CSRF_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
-X_FRAME_OPTIONS = "DENY"
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 # EMAIL
 # ------------------------------------------------------------------------------
