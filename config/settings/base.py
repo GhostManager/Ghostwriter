f31401daf164efecac5ccc7b9804eda7544ed90c
Christopher Maddalena <chris.maddalena@protonmail.com>
2020-08-21 19:38:27 -0700

    Removed django-tinymce

--- config/settings/base.py
+++ config/settings/base.py
@@ -372,10 +372,3 @@ CLOUD_SERVICE_CONFIG = {
     "aws_secret": env("AWS_SECRET", default=None),
     "do_api_key": env("DO_API_KEY", default=None),
 }
-
-# TINYMCE
-# ------------------------------------------------------------------------------
-TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, "js/tiny_mce")
-TINYMCE_JS_URL = os.path.join(STATIC_URL, "js/tiny_mce/tiny_mce.min.js")
-TINYMCE_SPELLCHECKER = True
-TINYMCE_COMPRESSOR = True
