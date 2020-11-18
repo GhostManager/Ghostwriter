2b168eadb30e36e58c085b8197eb5c416b114d91
Christopher Maddalena <chris.maddalena@protonmail.com>
2020-11-18 14:01:01 -0800

    Initial support for user groups
    Added a custom admin form for adding users to groups and granting specific permissions. Group memberships are listed on the user's profile page.

--- ghostwriter/users/forms.py
+++ ghostwriter/users/forms.py
@@ -1,6 +1,10 @@
 """This contains all of the forms used by the Users application."""
 
+# Django & Other 3rd Party Libraries
+from django.forms import ModelForm, ModelMultipleChoiceField
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth import forms, get_user_model
+from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
@@ -37,3 +41,40 @@ class UserCreationForm(forms.UserCreationForm):
             return username
 
         raise ValidationError(self.error_messages["duplicate_username"])
+
+
+# Create ModelForm based on the Group model.
+class GroupAdminForm(ModelForm):
+    class Meta:
+        model = Group
+        exclude = []
+
+    # Add the users field
+    users = ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        # Use the pretty ``filter_horizontal`` widget
+        widget=FilteredSelectMultiple("users", False),
+        label=_(
+            "Users",
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Do the normal form initialisation
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+        # If it is an existing group (saved objects have a pk)
+        if self.instance.pk:
+            # Populate the users field with the current Group users
+            self.fields["users"].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        # Add the users to the Group
+        self.instance.user_set.set(self.cleaned_data["users"])
+
+    def save(self, *args, **kwargs):
+        # Default save
+        instance = super(GroupAdminForm, self).save()
+        # Save many-to-many data
+        self.save_m2m()
+        return instance
