e4b4f779028c8a574a0b3da62abc0728f83ebddd
Christopher Maddalena <cmaddalena@specterops.io>
2021-02-12 11:20:07 -0800

    Added new template filter
    Created a filter for checking days between current date and a datetime value

--- ghostwriter/rolodex/templatetags/determine_primary.py
+++ ghostwriter/rolodex/templatetags/determine_primary.py
@@ -17,6 +17,11 @@ def get_primary_address(value):
     """
     Get the primary IP address for an individual :model:`shepherd.StaticServer`
     from :model:`shepherd.AuxServerAddress`.
+
+    **Parameters**
+
+    ``value``
+        Individual :model:`shepherd.StaticServer` entry
     """
     primary_address = value.ip_address
     aux_addresses = AuxServerAddress.objects.filter(static_server=value)
@@ -30,6 +35,13 @@ def get_primary_address(value):
 def get_scope_preview(value, n):
     """
     Get the top N lines of a ``scope`` list for an individual :model:`rolodex.ProjectScope`.
+
+    **Parameters**
+
+    ``value``
+        The ``scope`` value of an individual :model:`rolodex.ProjectScope` entry
+    ``value``
+        Number of lines to return
     """
     return "\n".join(value.split("\r\n")[0:n])
 
@@ -42,6 +54,21 @@ def plus_days(value, days):
     **Parameters**
 
     ``days``
-        A whole integer to add to the day value of a ``datetime`` value.
+        A whole integer to add to the day value of a ``datetime`` value
     """
     return value + datetime.timedelta(days=days)
+
+
+@register.filter
+def days_left(value):
+    """
+    Calculate how many days between the current date and a provide ``datetime`` value.
+
+    **Parameters**
+
+    ``value``
+        A ``datetime`` value
+    """
+    today = datetime.date.today()
+    delta = value - today
+    return delta.days
