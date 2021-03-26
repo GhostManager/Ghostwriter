07f5350c31bae794d9916585c46716e9f849abd8
Christopher Maddalena <cmaddalena@specterops.io>
2021-03-26 11:56:26 -0700

    Enabled Django Debug Toolbar by default

--- config/settings/local.py
+++ config/settings/local.py
@@ -37,18 +37,19 @@ EMAIL_PORT = 1025
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-# INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-# DEBUG_TOOLBAR_CONFIG = {
-#     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-#     "SHOW_TEMPLATE_CONTEXT": True,
-# }
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 if env("USE_DOCKER") == "yes":
+    # Standard Libraries
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
