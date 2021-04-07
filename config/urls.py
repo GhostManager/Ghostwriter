a469af706ad8f859d9def161814c6154c39ec6a4
Christopher Maddalena <cmaddalena@specterops.io>
2021-04-06 21:01:12 -0700

    Updated static file URLs for local development with Uvicorn

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,15 @@
 """This contains all of the base URL mappings used by Ghostwriter."""
 
-# Django & Other 3rd Party Libraries
+# Django Imports
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
 from django.views.generic import RedirectView
 
 # Ghostwriter Libraries
-from ghostwriter.home.views import (
-    protected_serve,
-)
+from ghostwriter.home.views import protected_serve
 from ghostwriter.users.views import (
     account_change_password,
     account_reset_password_from_key,
@@ -44,14 +43,17 @@ urlpatterns = [
         r"^%s(?P<path>.*)$" % settings.MEDIA_URL[1:],
         protected_serve,
         {"document_root": settings.MEDIA_ROOT},
-    )
+    ),
     # Add additional custom paths below this line...
     # Your stuff: custom urls includes go here
 ]
 
 if settings.DEBUG:
+    # Static file serving when using Gunicorn + Uvicorn for local web socket development
+    urlpatterns += staticfiles_urlpatterns()
+
     # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
+    # these url in browser to see how these error pages look like
     urlpatterns += [
         path(
             "400/",
@@ -71,6 +73,7 @@ if settings.DEBUG:
         path("500/", default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
+
+        import debug_toolbar  # noqa isort:skip
 
         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
