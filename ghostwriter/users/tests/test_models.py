fa8ef2f2830b4846e2fbf26586112bd072041a2c
Christopher Maddalena <cmaddalena@specterops.io>
2021-07-08 10:25:30 -0700

    Deleted old pytest tests

--- ghostwriter/users/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from django.conf import settings
-
-pytestmark = pytest.mark.django_db
-
-
-def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
-    assert user.get_absolute_url() == f"/users/{user.username}/"
