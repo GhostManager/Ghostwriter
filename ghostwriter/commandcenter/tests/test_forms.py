27ae2d6bf4c0e85d046f8749a21a61d4127f2f44
Christopher Maddalena <cmaddalena@specterops.io>
2021-07-23 12:52:00 -0700

    Initial commit of Command Center test cases

---
+++ ghostwriter/commandcenter/tests/test_forms.py
@@ -0,0 +1,92 @@
+# Standard Libraries
+import json
+import logging
+
+# Django Imports
+from django.test import TestCase
+
+# Ghostwriter Libraries
+from ghostwriter.commandcenter.forms import ReportConfigurationForm
+from ghostwriter.factories import (
+    ReportConfigurationFactory,
+    ReportDocxTemplateFactory,
+    ReportPptxTemplateFactory,
+)
+
+logging.disable(logging.INFO)
+
+
+class ReportConfigurationFormTests(TestCase):
+    """Collection of tests for :form:`commandcenter.ReportConfigurationForm`."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.config = ReportConfigurationFactory()
+        cls.docx_template = ReportDocxTemplateFactory()
+        cls.pptx_template = ReportPptxTemplateFactory()
+
+    def setUp(self):
+        pass
+
+    def form_data(
+        self,
+        enable_borders=None,
+        border_weight=None,
+        border_color=None,
+        prefix_figure=None,
+        label_figure=None,
+        prefix_table=None,
+        label_table=None,
+        default_docx_template_id=None,
+        default_pptx_template_id=None,
+        **kwargs,
+    ):
+        return ReportConfigurationForm(
+            data={
+                "enable_borders": enable_borders,
+                "border_weight": border_weight,
+                "border_color": border_color,
+                "prefix_figure": prefix_figure,
+                "label_figure": label_figure,
+                "prefix_table": prefix_table,
+                "label_table": label_table,
+                "default_docx_template": default_docx_template_id,
+                "default_pptx_template": default_pptx_template_id,
+            },
+        )
+
+    def test_valid_data(self):
+        form = self.form_data(**self.config.__dict__)
+        self.assertTrue(form.is_valid())
+
+    def test_clean_default_docx_template(self):
+        config = self.config.__dict__.copy()
+        form = self.form_data(**config)
+        self.assertTrue(form.is_valid())
+
+        self.docx_template.lint_result = json.dumps(
+            {"result": "failed", "warnings": [], "errors": []}
+        )
+        self.docx_template.save()
+        config["default_docx_template_id"] = self.docx_template.pk
+
+        form = self.form_data(**config)
+        errors = form.errors["default_docx_template"].as_data()
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0].code, "invalid")
+
+    def test_clean_default_pptx_template(self):
+        config = self.config.__dict__.copy()
+        form = self.form_data(**config)
+        self.assertTrue(form.is_valid())
+
+        self.pptx_template.lint_result = json.dumps(
+            {"result": "failed", "warnings": [], "errors": []}
+        )
+        self.pptx_template.save()
+        config["default_pptx_template_id"] = self.pptx_template.pk
+
+        form = self.form_data(**config)
+        errors = form.errors["default_pptx_template"].as_data()
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0].code, "invalid")
