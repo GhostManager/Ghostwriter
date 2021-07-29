617aa6711e3ae0a0b68c8acf139721dbba2da182
Christopher Maddalena <cmaddalena@specterops.io>
2021-07-29 16:56:42 -0700

    Initial commit of test cases

---
+++ ghostwriter/singleton/tests/test_views.py
@@ -0,0 +1,32 @@
+# Standard Libraries
+import logging
+from datetime import date, timedelta
+
+# Django Imports
+from django.test import TestCase
+
+# Ghostwriter Libraries
+from ghostwriter.factories import CompanyInformationFactory
+from ghostwriter.singleton.templatetags import settings_tags
+
+logging.disable(logging.INFO)
+
+PASSWORD = "SuperNaturalReporting!"
+
+
+# Tests related to custom template tags and filters
+
+
+class TemplateTagTests(TestCase):
+    """Collection of tests for custom template tags."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.company = CompanyInformationFactory(company_name="SpecterOps")
+
+    def setUp(self):
+        pass
+
+    def test_tags(self):
+        result = settings_tags.get_solo("commandcenter.CompanyInformation")
+        self.assertEqual(result.company_name, "SpecterOps")
