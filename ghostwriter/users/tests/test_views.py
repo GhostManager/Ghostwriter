3925ddaf6e258d3bdbf5ceee218c80e89f738dbe
Christopher Maddalena <cmaddalena@specterops.io>
2021-09-15 12:02:50 -0700

    Added tests for some user mgmt views

---
+++ ghostwriter/users/tests/test_views.py
@@ -0,0 +1,114 @@
+# Standard Libraries
+import logging
+
+# Django Imports
+from django.test import Client, TestCase
+from django.urls import reverse
+
+# Ghostwriter Libraries
+from ghostwriter.factories import UserFactory
+
+logging.disable(logging.INFO)
+
+PASSWORD = "SuperNaturalReporting!"
+
+
+class UserDetailViewTests(TestCase):
+    """Collection of tests for :view:`users.UserDetailView`."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("users:detail", kwargs={"username": cls.user.username})
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
+
+    def test_view_uri_exists_at_desired_location(self):
+        response = self.client_auth.get(self.uri)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_requires_login(self):
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 302)
+
+
+class UserUpdateViewTests(TestCase):
+    """Collection of tests for :view:`users.UserUpdateView`."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("users:update")
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
+
+    def test_view_uri_exists_at_desired_location(self):
+        response = self.client_auth.get(self.uri)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_requires_login(self):
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 302)
+
+
+class UserRedirectViewTests(TestCase):
+    """Collection of tests for :view:`users.UserRedirectView`."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("users:redirect")
+        cls.redirect_uri = reverse("users:detail", kwargs={"username": cls.user.username})
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
+
+    def test_view_uri_exists_at_desired_location(self):
+        response = self.client_auth.get(self.uri)
+        self.assertRedirects(response, self.redirect_uri)
+
+    def test_view_requires_login(self):
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 302)
+
+
+class GhostwriterPasswordChangeViewTests(TestCase):
+    """Collection of tests for :view:`users.GhostwriterPasswordChangeView`."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("account_change_password")
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
+
+    def test_view_uri_exists_at_desired_location(self):
+        response = self.client_auth.get(self.uri)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_requires_login(self):
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 302)
