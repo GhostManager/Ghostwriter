b8fa24a42454e42798fda2126f75d7e7912d7608
Christopher Maddalena <cmaddalena@specterops.io>
2022-02-04 11:46:02 -0800

    Updated version info

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,9 +10,9 @@ from django.contrib.messages import constants as messages
 # 3rd Party Libraries
 import environ
 
-__version__ = "2.2.3-rc"
+__version__ = "2.2.3-rc2"
 VERSION = __version__
-RELEASE_DATE = "28 Jan 2022"
+RELEASE_DATE = "Feb 2022"
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = ROOT_DIR / "ghostwriter"
