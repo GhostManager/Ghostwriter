ec766dad565455b040639988580c2bb9749d1480
Christopher Maddalena <cmaddalena@specterops.io>
2022-02-07 15:29:21 -0800

    Set `DEFAULT_AUTO_FIELD` for models to `BigAutoField` for Django 3.2

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,6 +51,8 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
