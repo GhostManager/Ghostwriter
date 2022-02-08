dd39c638440a1d9449a06b4ec6ad9a4b7fd5132c
Christopher Maddalena <cmaddalena@specterops.io>
2022-02-08 10:56:49 -0800

    Fixed an incorrect `get_absolute_url()` method

--- ghostwriter/reporting/models.py
+++ ghostwriter/reporting/models.py
@@ -281,7 +281,7 @@ class ReportTemplate(models.Model):
         verbose_name_plural = "Report templates"
 
     def get_absolute_url(self):
-        return reverse("reporting:template_file", args=[str(self.id)])
+        return reverse("reporting:template_detail", args=[str(self.id)])
 
     def __str__(self):
         return f"{self.name}"
