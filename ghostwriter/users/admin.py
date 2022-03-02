702de50d8f3d0baf293e79a682ae6d606cf226d3
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-03-02 11:17:42 -0800

    Added ``role`` field to admin panel

--- ghostwriter/users/admin.py
+++ ghostwriter/users/admin.py
@@ -38,7 +38,7 @@ class UserAdmin(auth_admin.UserAdmin):
     )
 
     fieldsets = (
-        (_("User Information"), {"fields": ("username", "password")}),
+        (_("User Information"), {"fields": ("username", "role", "password")}),
         (_("Personal Information"), {"fields": ("name", "email", "phone", "timezone")}),
         (
             _("User Permissions"),
