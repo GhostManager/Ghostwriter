1f4e5423720795d0fa17a00628e3053b6981c196
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-03-30 14:58:48 -0700

    Organized GraphQL unit tests into separate classes

--- ghostwriter/graphql/tests/test_utils.py
+++ ghostwriter/graphql/tests/test_utils.py
@@ -22,14 +22,6 @@ class JwtUtilsTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = UserFactory(password=PASSWORD)
-        cls.webhook_uri = reverse("graphql_webhook")
-        cls.login_uri = reverse("graphql_login")
-        cls.whoami_uri = reverse("graphql_whoami")
-        cls.public_data = {
-            "X-Hasura-Role": "public",
-            "X-Hasura-User-Id": "-1",
-            "X-Hasura-User-Name": "anonymous",
-        }
 
     def setUp(self):
         self.client = Client()
@@ -44,18 +36,6 @@ class JwtUtilsTests(TestCase):
             payload, encoded_payload = utils.generate_jwt(self.user)
         except Exception:
             self.fail("generate_jwt() raised an AttributeError unexpectedly!")
-        try:
-            self.assertTrue(utils.verify_jwt_user(payload["https://hasura.io/jwt/claims"]))
-        except Exception:
-            self.fail("verify_jwt_user() raised an AttributeError unexpectedly!")
-        try:
-            self.assertTrue(utils.get_jwt_payload(encoded_payload))
-        except Exception:
-            self.fail("get_jwt_payload() raised an AttributeError unexpectedly!")
-        try:
-            self.assertTrue(utils.verify_hasura_claims(payload))
-        except Exception:
-            self.fail("verify_hasura_claims() raised an AttributeError unexpectedly!")
 
     def test_generate_jwt_with_expiration(self):
         expiration = datetime(2099, 1, 1).timestamp()
@@ -64,117 +44,54 @@ class JwtUtilsTests(TestCase):
         )
         self.assertTrue(payload["exp"], expiration)
 
-    def test_graphql_login(self):
-        data = {
-            "input": {"username": F"{self.user.username}", "password": f"{PASSWORD}"}
-        }
-        response = self.client.post(
-            self.login_uri,
-            data=data,
-            content_type="application/json",
-            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
-        )
-        self.assertEqual(response.status_code, 200)
-        # Test bypasses Hasura so the ``["data"]["login"]`` keys are not present
-        self.assertTrue(response.json()["token"])
+    def test_verify_jwt(self):
+        payload, encoded_payload = utils.generate_jwt(self.user)
+        try:
+            self.assertTrue(utils.verify_jwt_user(payload["https://hasura.io/jwt/claims"]))
+        except Exception:
+            self.fail("verify_jwt_user() raised an AttributeError unexpectedly!")
 
-    def test_graphql_login_with_invalid_credentials(self):
-        data = {
-            "input": {"username": F"{self.user.username}", "password": "Not the Password"}
-        }
-        result = {
-            "message": "Invalid credentials",
-            "extensions": {"code": "InvalidCredentials", },
-        }
-        response = self.client.post(
-            self.login_uri,
-            data=data,
-            content_type="application/json",
-            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
-        )
-        self.assertEqual(response.status_code, 401)
-        self.assertJSONEqual(force_str(response.content), result)
+    def test_get_jwt_payload(self):
+        payload, encoded_payload = utils.generate_jwt(self.user)
+        try:
+            self.assertTrue(utils.get_jwt_payload(encoded_payload))
+        except Exception:
+            self.fail("get_jwt_payload() raised an AttributeError unexpectedly!")
 
-    def test_graphql_login_requires_secret(self):
-        data = {
-            "input": {"username": F"{self.user.username}", "password": f"{PASSWORD}"}
-        }
-        response = self.client.post(
-            self.login_uri,
-            data=data,
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, 403)
+    def test_verify_hasura_claims(self):
+        payload, encoded_payload = utils.generate_jwt(self.user)
+        try:
+            self.assertTrue(utils.verify_hasura_claims(payload))
+        except Exception:
+            self.fail("verify_hasura_claims() raised an AttributeError unexpectedly!")
 
-    def test_graphql_login_rejects_bad_request(self):
-        data = {
-            "bad_input": {"username": F"{self.user.username}", "password": f"{PASSWORD}"}
-        }
-        response = self.client.post(
-            self.login_uri,
-            data=data,
-            content_type="application/json",
-            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
-        )
-        self.assertEqual(response.status_code, 400)
-
-    def test_graphql_login_without_secret(self):
-        data = {
-            "input": {"username": F"{self.user.username}", "password": F"{PASSWORD}"}
-        }
-        response = self.client.post(
-            self.login_uri,
-            data=data,
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, 403)
-
-        result = {
-            "message": "Unauthorized access method",
-            "extensions": {"code": "Unauthorized", },
-        }
-        self.assertJSONEqual(force_str(response.content), result)
-
-    def test_graphql_whoami(self):
+    def test_verify_hasura_claims_without_claims(self):
         payload, token = utils.generate_jwt(self.user)
-        response = self.client.post(
-            self.whoami_uri,
-            content_type="application/json",
-            **{"HTTP_HASURA_ACTION_SECRET": "changeme", "HTTP_AUTHORIZATION": f"Bearer {token}"},
-        )
-        self.assertEqual(response.status_code, 200)
-        # Test bypasses Hasura so the ``["data"]["whoami"]`` keys are not present
-        self.assertEqual(response.json()["username"], self.user.username)
+        del payload["https://hasura.io/jwt/claims"]
+        result = utils.verify_hasura_claims(payload)
+        self.assertFalse(result)
 
-    def test_graphql_whoami_rejects_missing_jwt(self):
-        response = self.client.post(
-            self.whoami_uri,
-            content_type="application/json",
-            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
-        )
-        self.assertEqual(response.status_code, 400)
 
-    def test_graphql_whoami_requires_valid_jwt(self):
-        token = "GARBAGE!"
-        response = self.client.post(
-            self.whoami_uri,
-            content_type="application/json",
-            **{"HTTP_HASURA_ACTION_SECRET": "changeme", "HTTP_AUTHORIZATION": f"Bearer {token}"},
-        )
-        self.assertEqual(response.status_code, 401)
+class HasuraWebhookTests(TestCase):
+    """Collection of tests for the `users:graphql_webhook` view."""
 
-    def test_graphql_whoami_without_secret(self):
-        response = self.client.post(
-            self.whoami_uri,
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, 403)
-
-        result = {
-            "message": "Unauthorized access method",
-            "extensions": {"code": "Unauthorized", },
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("graphql_webhook")
+        cls.public_data = {
+            "X-Hasura-Role": "public",
+            "X-Hasura-User-Id": "-1",
+            "X-Hasura-User-Name": "anonymous",
         }
-        self.assertJSONEqual(force_str(response.content), result)
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
 
     def test_graphql_webhook_with_valid_jwt(self):
         payload, token = utils.generate_jwt(self.user)
@@ -184,7 +101,7 @@ class JwtUtilsTests(TestCase):
             "X-Hasura-User-Name": f"{self.user.username}",
         }
         response = self.client.get(
-            self.webhook_uri,
+            self.uri,
             content_type="application/json",
             **{"HTTP_AUTHORIZATION": f"Bearer {token}", },
         )
@@ -196,7 +113,7 @@ class JwtUtilsTests(TestCase):
         self.user.is_active = False
         self.user.save()
         response = self.client.get(
-            self.webhook_uri,
+            self.uri,
             content_type="application/json",
             **{"HTTP_AUTHORIZATION": f"Bearer {token}", },
         )
@@ -208,7 +125,7 @@ class JwtUtilsTests(TestCase):
     def test_graphql_webhook_with_invalid_jwt(self):
         token = "GARBAGE!"
         response = self.client.get(
-            self.webhook_uri,
+            self.uri,
             content_type="application/json",
             **{"HTTP_AUTHORIZATION": f"Bearer {token}", },
         )
@@ -216,17 +133,156 @@ class JwtUtilsTests(TestCase):
         self.assertJSONEqual(force_str(response.content), self.public_data)
 
     def test_graphql_webhook_without_claims(self):
-        payload, token = utils.generate_jwt(self.user)
-        del payload["https://hasura.io/jwt/claims"]
+        payload, token = utils.generate_jwt(self.user, exclude_hasura=True)
         response = self.client.get(
-            self.webhook_uri,
+            self.uri,
             content_type="application/json",
             **{"HTTP_AUTHORIZATION": f"Bearer {token}", },
         )
         self.assertEqual(response.status_code, 401)
 
-    def test_verify_hasura_claims_without_claims(self):
+
+class HasuraLoginTests(TestCase):
+    """Collection of tests for the `users:graphql_login` view."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("graphql_login")
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
+
+    def test_graphql_login(self):
+        data = {
+            "input": {"username": f"{self.user.username}", "password": f"{PASSWORD}"}
+        }
+        response = self.client.post(
+            self.uri,
+            data=data,
+            content_type="application/json",
+            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
+        )
+        self.assertEqual(response.status_code, 200)
+        # Test bypasses Hasura so the ``["data"]["login"]`` keys are not present
+        self.assertTrue(response.json()["token"])
+
+    def test_graphql_login_with_invalid_credentials(self):
+        data = {
+            "input": {"username": f"{self.user.username}", "password": "Not the Password"}
+        }
+        result = {
+            "message": "Invalid credentials",
+            "extensions": {"code": "InvalidCredentials", },
+        }
+        response = self.client.post(
+            self.uri,
+            data=data,
+            content_type="application/json",
+            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertJSONEqual(force_str(response.content), result)
+
+    def test_graphql_login_requires_secret(self):
+        data = {
+            "input": {"username": f"{self.user.username}", "password": f"{PASSWORD}"}
+        }
+        response = self.client.post(
+            self.uri,
+            data=data,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_graphql_login_rejects_bad_request(self):
+        data = {
+            "bad_input": {"username": f"{self.user.username}", "password": f"{PASSWORD}"}
+        }
+        response = self.client.post(
+            self.uri,
+            data=data,
+            content_type="application/json",
+            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_graphql_login_without_secret(self):
+        data = {
+            "input": {"username": f"{self.user.username}", "password": f"{PASSWORD}"}
+        }
+        response = self.client.post(
+            self.uri,
+            data=data,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 403)
+
+        result = {
+            "message": "Unauthorized access method",
+            "extensions": {"code": "Unauthorized", },
+        }
+        self.assertJSONEqual(force_str(response.content), result)
+
+
+class HasuraWhoamiTests(TestCase):
+    """Collection of tests for the `users:graphql_whoami` view."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(password=PASSWORD)
+        cls.uri = reverse("graphql_whoami")
+
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.client_auth.login(username=self.user.username, password=PASSWORD)
+        self.assertTrue(
+            self.client_auth.login(username=self.user.username, password=PASSWORD)
+        )
+
+    def test_graphql_whoami(self):
         payload, token = utils.generate_jwt(self.user)
-        del payload["https://hasura.io/jwt/claims"]
-        result = utils.verify_hasura_claims(payload)
-        self.assertFalse(result)
+        response = self.client.post(
+            self.uri,
+            content_type="application/json",
+            **{"HTTP_HASURA_ACTION_SECRET": "changeme", "HTTP_AUTHORIZATION": f"Bearer {token}"},
+        )
+        self.assertEqual(response.status_code, 200)
+        # Test bypasses Hasura so the ``["data"]["whoami"]`` keys are not present
+        self.assertEqual(response.json()["username"], self.user.username)
+
+    def test_graphql_whoami_rejects_missing_jwt(self):
+        response = self.client.post(
+            self.uri,
+            content_type="application/json",
+            **{"HTTP_HASURA_ACTION_SECRET": "changeme", },
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_graphql_whoami_requires_valid_jwt(self):
+        token = "GARBAGE!"
+        response = self.client.post(
+            self.uri,
+            content_type="application/json",
+            **{"HTTP_HASURA_ACTION_SECRET": "changeme", "HTTP_AUTHORIZATION": f"Bearer {token}"},
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_graphql_whoami_without_secret(self):
+        response = self.client.post(
+            self.uri,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 403)
+
+        result = {
+            "message": "Unauthorized access method",
+            "extensions": {"code": "Unauthorized", },
+        }
+        self.assertJSONEqual(force_str(response.content), result)
