cb48bd12d37bb63d7f29e37e18831a525b4cacfc
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-07-06 09:52:33 -0700

    Allowed oplog entry fields to be null for inserts via GraphQL

--- ghostwriter/oplog/models.py
+++ ghostwriter/oplog/models.py
@@ -59,34 +59,39 @@ class OplogEntry(models.Model):
     end_date = models.DateTimeField(null=True, blank=True)
     source_ip = models.TextField(
         "Source IP / Hostname",
+        null=True,
         blank=True,
         help_text="Provide the source hostname / IP from which the command originated.",
     )
 
     dest_ip = models.TextField(
         "Destination IP/Hostname",
+        null=True,
         blank=True,
         help_text="Provide the destination hostname / ip on which the command was ran.",
     )
 
     tool = models.TextField(
-        "Tool name", blank=True, help_text="The tool used to execute the action"
+        "Tool name", null=True, blank=True, help_text="The tool used to execute the action",
     )
 
     user_context = models.TextField(
         "User Context",
+        null=True,
         blank=True,
         help_text="The user context that executed the command",
     )
 
     command = models.TextField(
         "Command",
+        null=True,
         blank=True,
         help_text="The command that was executed",
     )
 
     description = models.TextField(
         "Description",
+        null=True,
         blank=True,
         help_text="A description of why the command was executed and expected results.",
     )
@@ -100,12 +105,14 @@ class OplogEntry(models.Model):
 
     comments = models.TextField(
         "Comments",
+        null=True,
         blank=True,
         help_text="Any additional comments or useful information.",
     )
 
     operator_name = models.CharField(
         "Operator",
+        null=True,
         blank=True,
         help_text="The operator that performed the action.",
         max_length=255,
