00bb4544ba9679fcfa16de0a9343f7b9799825f6
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-09-12 15:18:31 -0700

    Fixed Slack channel check for new projects

--- ghostwriter/rolodex/signals.py
+++ ghostwriter/rolodex/signals.py
@@ -42,7 +42,7 @@ def update_project(sender, instance, **kwargs):
     Updates dates for :model:`shepherd.History`, :model:`shepherd.ServerHistory`, and
     :model:`rolodex.ProjectAssignments` whenever the :model:`rolodex.Project` is updated.
     """
-    if instance.initial_slack_channel:
+    if instance.slack_channel:
         slack = SlackNotification()
 
     if kwargs["created"]:
