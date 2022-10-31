467f381ce22199b3cb2371cd88211dbc92c24750
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-10-31 10:24:01 -0700

    Updated version info for v3.1.2

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,9 +11,9 @@ from django.contrib.messages import constants as messages
 # 3rd Party Libraries
 import environ
 
-__version__ = "3.1.1"
+__version__ = "3.1.2"
 VERSION = __version__
-RELEASE_DATE = "18 October 2022"
+RELEASE_DATE = "31 October 2022"
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = ROOT_DIR / "ghostwriter"
