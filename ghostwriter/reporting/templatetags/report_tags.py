4bb3986b17a6af6b71861ec4e6fc80c014c3e1e5
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-12-14 15:40:55 -0800

    Updated to use severity weights instead of names for UI styling

--- ghostwriter/reporting/templatetags/report_tags.py
+++ ghostwriter/reporting/templatetags/report_tags.py
@@ -45,8 +45,12 @@ def group_by_severity(queryset):
     all_severity = Severity.objects.all().order_by("weight")
     severity_dict = defaultdict(list)
     for severity in all_severity:
-        severity_dict[str(severity)] = []
+        severity_dict[str(severity)] = {
+            "tpl_name": f"severity_{severity.weight}",
+            "findings": [],
+            "weight": severity.weight,
+        }
     for finding in queryset:
-        severity_dict[str(finding.severity)].append(finding)
+        severity_dict[str(finding.severity)]["findings"].append(finding)
     # Return a basic dict because templates can't handle defaultdict
     return dict(severity_dict)
