34647b777b802c6bacd00088d603997d68de8cf9
Christopher Maddalena <chris.maddalena@protonmail.com>
2023-01-25 16:20:55 -0800

    Added preview of user profile picture uploads

--- ghostwriter/home/forms.py
+++ ghostwriter/home/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 
 # 3rd Party Libraries
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import HTML, ButtonHolder, Div, Layout, Submit
+from crispy_forms.layout import HTML, ButtonHolder, Div, Field, Layout, Submit
 
 # Ghostwriter Libraries
 from ghostwriter.home.models import UserProfile
@@ -36,23 +36,32 @@ class UserProfileForm(forms.ModelForm):
             HTML(
                 """
                 <h4 class="icon avatar-upload-icon">Avatar Upload</h4>
+                <hr>
                 <p>Your new avatar should be a square (equal height and width) for the best looking results.</p>
+                Previews for images will appear below.</p>
+                <div id="avatarPreview" class="pb-3"></div>
                 """
             ),
             Div(
-                "avatar",
+                Field(
+                    "avatar",
+                    id="id_avatar",
+                    css_class="custom-file-input",
+                ),
                 HTML(
                     """
-                    <label id="filename" class="custom-file-label" for="customFile">Choose an Avatar Image File...</label>
+                    <label id="filename" class="custom-file-label" for="customFile">
+                    Click here to select or drag and drop your profile image...</label>
                     """
                 ),
-                css_class="custom-file col-md-8",
+                css_class="custom-file col-8",
             ),
             ButtonHolder(
                 Submit("submit", "Submit", css_class="btn btn-primary col-md-4"),
                 HTML(
                     """
-                    <button onclick="window.location.href='{{ cancel_link }}'" class="btn btn-outline-secondary col-md-4" type="button">Cancel</button>
+                    <button onclick="window.location.href='{{ cancel_link }}'"
+                    class="btn btn-outline-secondary col-md-4" type="button">Cancel</button>
                     """
                 ),
             ),
