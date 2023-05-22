be8194ca97032527d1e5d8f536842f22c5a0223d
Christopher Maddalena <chris.maddalena@protonmail.com>
2023-05-22 16:24:02 -0700

    Allow checkouts of domains past expiration with auto-renew enabled

--- ghostwriter/shepherd/forms.py
+++ ghostwriter/shepherd/forms.py
@@ -134,7 +134,7 @@ class CheckoutForm(forms.ModelForm):
         if insert:
             unavailable = DomainStatus.objects.get(domain_status="Unavailable")
             expired = domain.expiration < date.today()
-            if expired:
+            if expired and domain.auto_renew is False:
                 raise ValidationError(_("This domain has expired!"), code="expired")
             if domain.domain_status == unavailable:
                 raise ValidationError(
