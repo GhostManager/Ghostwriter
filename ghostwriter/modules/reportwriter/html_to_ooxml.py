a273687fa8941085a0ecbcd3c5931676625b0259
Alexander Parrill <aparrill@specterops.io>
2024-02-14 13:21:48 -0500

    Refactor rich text handling in the report writer
    Split the rich text HTML to docx/pptx converting code into their own
    files: a `BaseHtmlToOOXML` for implementing common code, inherited by
    `HtmlToDocx` and `HtmlToPptx` for docx and pptx specific functionality,
    then by `HtmlToDocxWithEvidence` and `HtmlToPptxWithEvidence` that adds
    Ghostwriter-specific figure and evidence handling.
    
    The `BaseHtmlToOOXML` operates by recursively traversing the HTML DOM,
    calling the `tag_<tagname>` methods on the class if they exist on
    encountering a tag, or the `text` method for text nodes. This approach
    easily separates out the handling of each tag into individual methods,
    as well as allow subclasses to change behaviors by overriding. The
    handlers also get passed a set of kwargs, allowing to pass data down the
    tree - usually, the styles dictionary and the paragraph object.

---
+++ ghostwriter/modules/reportwriter/html_to_ooxml.py
@@ -0,0 +1,148 @@
+
+import re
+import logging
+
+import bs4
+
+logger = logging.getLogger(__name__)
+
+
+def set_style_method(tag_name, style_key, style_value=True):
+    """
+    Creates and returns a `tag_*` method that sets a value in the `styles` dict
+    and then recurses into its children.
+    """
+    def tag_style(self, el, style={}, **kwargs):
+        style = style.copy()
+        style[style_key] = style_value
+        self.process_children(el.children, style=style, **kwargs)
+    tag_style.__name__ = "tag_" + tag_name
+    return tag_style
+
+
+class BaseHtmlToOOXML:
+    """
+    Base HTML to OpenOffice XML converter. Converts HTML from the TinyMCE rich
+    text to various types of OpenXML documents.
+
+    Use a subclass that matches the desired document type.
+    """
+
+    @classmethod
+    def run(cls, text: str, *args, **kwargs):
+        """
+        Parses the `text` as HTML and runs the class over the tree.
+        Extra parameters are passed to the class's `__init__`.
+        """
+        soup = bs4.BeautifulSoup(text, "lxml")
+        tag = soup.find("body")
+        instance = cls(*args, **kwargs)
+        if tag is not None:
+            instance.process_children(tag.children)
+        return instance
+
+    def process(self, el, **kwargs):
+        if el.name:
+            if hasattr(self, "tag_" + el.name):
+                getattr(self, "tag_" + el.name)(el, **kwargs)
+            else:
+                logger.warning("Unimplemented tag: %s, skipping", el.name)
+        else:
+            self.text(el, **kwargs)
+
+    def process_children(self, children_iterable, **kwargs):
+        for ch in children_iterable:
+            self.process(ch, **kwargs)
+
+    def text(self, el, par=None, style=None, **kwargs):
+        if par is None:
+            # Text without a paragraph. If this is just some trailing whitespace, ignore it, otherwise
+            # report an error.
+            if el.text.strip():
+                raise ValueError("found text node that was not enclosed in a paragraph or other block item: {!r}".format(el.text))
+            else:
+                return
+        text = self.strip_text_whitespace(el.text)
+        if not text:
+            return
+        run = par.add_run()
+        run.text = text
+        self.style_run(run, style or {})
+
+    def style_run(self, run, style):
+        """
+        Called by the default `text` method to style a run. Overridable to
+        extend with additional styles.
+        """
+        if style.get("bold"):
+            run.font.bold = True
+        if style.get("italic"):
+            run.font.italic = True
+        if style.get("underline"):
+            run.font.underline = True
+        if style.get("font_family"):
+            run.font.name = style["font_family"]
+        if style.get("font_size"):
+            run.font.size = "font_size"
+
+    def strip_text_whitespace(self, text):
+        """
+        Consolidates adjacent whitespace into one space, similar to how browsers display it
+        """
+        return re.sub(r"\s+", " ", text)
+
+    tag_code = set_style_method("code", "inline_code")
+    tag_b = set_style_method("b", "bold")
+    tag_strong = set_style_method("strong", "bold")
+    tag_i = set_style_method("i", "italic")
+    tag_em = set_style_method("em", "italic")
+    tag_u = set_style_method("u", "underline")
+    tag_sub = set_style_method("sub", "subscript")
+    tag_sup = set_style_method("sup", "superscript")
+    tag_del = set_style_method("del", "strikethrough")
+
+    def tag_a(self, el, style={}, **kwargs):
+        style = style | {"hyperlink_url": el.attrs.get("href")}
+        self.process_children(el.children, style=style, **kwargs)
+
+    def tag_span(self, el, style={}, **kwargs):
+        style = style.copy()
+
+        # Parse and check classes
+        classes = el.attrs.get("class", [])
+        for cls in ["italic", "bold", "underline", "highlight"]:
+            if cls in classes:
+                style[cls] = True
+
+        # Parse and check styles
+        for style_line in el.attrs.get("style", "").split(";"):
+            if not style_line.strip():
+                continue
+            try:
+                key, value = style_line.split(":")
+                key = key.strip()
+                value = value.strip()
+
+                if key == "font-size":
+                    style["font_size"] = float(value.replace("pt", ""))
+                elif key == "font-family":
+                    font_list = value.split(",")
+                    priority_font = font_list[0].replace("'", "").replace('"', "").strip()
+                    style["font_family"] = priority_font
+                elif key == "color" or key == "background-color":
+                    value = value.replace("#", "")
+                    r, g, b = (int(value[i * 2 : i * 2 + 2], 16) for i in range(3))
+                    if key == "color":
+                        style["font_color"] = (r, g, b)
+                    else:
+                        style["background_color"] = (r, g, b)
+
+            except (ValueError, IndexError):
+                # Invalid input
+                pass
+
+        self.process_children(el.children, style=style, **kwargs)
+
+    def tag_br(self, el, par, **kwargs):
+        run = par.add_run()
+        run.add_break()
---
+++ ghostwriter/reporting/tests/test_rich_text_pptx.py
@@ -0,0 +1,312 @@
+
+import pptx
+from io import BytesIO
+from zipfile import ZipFile
+
+from django.test import TestCase
+from .test_rich_text_docx import clean_xml
+
+from ghostwriter.modules.reportwriter.html_to_pptx import HtmlToPptx
+
+PPTX_PREFIX = """<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<p:sld xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
+    xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main"
+    xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+    <p:cSld>
+        <p:spTree>
+            <p:nvGrpSpPr>
+                <p:cNvPr id="1" name=""/>
+                <p:cNvGrpSpPr/>
+                <p:nvPr/>
+            </p:nvGrpSpPr>
+            <p:grpSpPr/>
+            <p:sp>
+                <p:nvSpPr>
+                    <p:cNvPr id="2" name="Title 1"/>
+                    <p:cNvSpPr>
+                        <a:spLocks noGrp="1"/>
+                    </p:cNvSpPr>
+                    <p:nvPr>
+                        <p:ph type="title"/>
+                    </p:nvPr>
+                </p:nvSpPr>
+                <p:spPr/>
+                <p:txBody>
+                    <a:bodyPr/>
+                    <a:lstStyle/>
+                    <a:p/>
+                </p:txBody>
+            </p:sp>
+            <p:sp>
+                <p:nvSpPr>
+                    <p:cNvPr id="3" name="Content Placeholder 2"/>
+                    <p:cNvSpPr>
+                        <a:spLocks noGrp="1"/>
+                    </p:cNvSpPr>
+                    <p:nvPr>
+                        <p:ph idx="1"/>
+                    </p:nvPr>
+                </p:nvSpPr>
+                <p:spPr/>
+                <p:txBody>
+                    <a:bodyPr/>
+                    <a:lstStyle/>
+"""
+PPTX_SUFFIX = """
+    </p:txBody>
+            </p:sp>
+        </p:spTree>
+    </p:cSld>
+    <p:clrMapOvr>
+        <a:masterClrMapping/>
+    </p:clrMapOvr>
+</p:sld>
+"""
+SLD_LAYOUT_TITLE_AND_CONTENT = 1
+
+
+def mk_test_pptx(name, input, expected_output):
+    expected_output = clean_xml(PPTX_PREFIX + expected_output + PPTX_SUFFIX)
+
+    def test_func(self):
+        ppt = pptx.Presentation()
+        slide = ppt.slides.add_slide(ppt.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT])
+        shape = slide.shapes.placeholders[1]
+        shape.text_frame.clear()
+        HtmlToPptx.run(input, slide, shape)
+
+        # pptx leaves an annoying empty paragraph at the start, remove it
+        prefix_par = shape.text_frame.paragraphs[0]._p
+        prefix_par.getparent().remove(prefix_par)
+
+        out = BytesIO()
+        ppt.part.save(out)
+
+        # Uncomment to write generates pptx files for manual inspection
+        with open(name + ".pptx", "wb") as f:
+            f.write(out.getvalue())
+
+        with ZipFile(out) as zip:
+            with zip.open("ppt/slides/slide1.xml") as file:
+                contents = file.read()
+        contents = clean_xml(contents)
+        self.assertEqual(contents, expected_output)
+    test_func.__name__ = name
+    return test_func
+
+
+class RichTextToPptxTests(TestCase):
+    maxDiff = None
+
+    test_paragraphs = mk_test_pptx(
+        "test_paragraphs",
+        "<p>Hello World!</p><p>This is a test!</p>",
+        """
+            <a:p><a:r><a:t>Hello World!</a:t></a:r></a:p>
+            <a:p><a:r><a:t>This is a test!</a:t></a:r></a:p>
+        """,
+    )
+
+    test_headers = mk_test_pptx(
+        "test_headers",
+        "<h1>Hello World!</h1><h2>Heading two</h2><h3>Heading three</h3><p>Paragraph</p>",
+        """
+            <a:p><a:r><a:rPr b="1" /><a:t>Hello World!</a:t></a:r></a:p>
+            <a:p><a:r><a:rPr b="1" /><a:t>Heading two</a:t></a:r></a:p>
+            <a:p><a:r><a:rPr b="1" /><a:t>Heading three</a:t></a:r></a:p>
+            <a:p><a:r><a:t>Paragraph</a:t></a:r></a:p>
+        """
+    )
+
+    test_formatting = mk_test_pptx(
+        "test_formatting",
+        """
+            <p>
+                <b>Bold</b>
+                <strong>Strong</strong>
+                <i>Italic</i>
+                <em>Emphasis</em>
+                <u>Underline</u>
+                <sub>Subscript</sub>
+                <sup>Superscript</sup>
+                <del>Strikethrough</del>
+            </p>
+        """,
+        """
+            <a:p>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr b="1" /><a:t>Bold</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr b="1" /><a:t>Strong</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr i="1" /><a:t>Italic</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr i="1" /><a:t>Emphasis</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr u="sng" /><a:t>Underline</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr baseline="-25000" /><a:t>Subscript</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr baseline="30000" /><a:t>Superscript</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+                <a:r><a:rPr strike="sngStrike" /><a:t>Strikethrough</a:t></a:r>
+                <a:r><a:t> </a:t></a:r>
+            </a:p>
+        """
+    )
+
+    test_unordered_list = mk_test_pptx(
+        "test_unordered_list",
+        """
+            <p>List test one:</p>
+            <ul>
+                <li>Item one</li>
+                <li>Item two</li>
+                <li>Item three:<ul>
+                    <li>Subitem one</li>
+                    <li>Subitem two</li>
+                </ul></li>
+            </ul>
+            <p>List test two:</p>
+            <ul>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ul>
+        """,
+        """
+            <a:p><a:r><a:t>List test one:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item two</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item three:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="2"/><a:r><a:t>Subitem one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="2"/><a:r><a:t>Subitem two</a:t></a:r></a:p>
+            <a:p><a:r><a:t>List test two:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item two</a:t></a:r></a:p>
+        """,
+    )
+
+    test_ordered_list = mk_test_pptx(
+        "test_ordered_list",
+        """
+            <p>List test one:</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+                <li>Item three:<ol>
+                    <li>Subitem one</li>
+                    <li>Subitem two</li>
+                </ol></li>
+            </ol>
+            <p>List test two:</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+        """,
+        """
+            <a:p><a:r><a:t>List test one:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item two</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item three:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="2"/><a:r><a:t>Subitem one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="2"/><a:r><a:t>Subitem two</a:t></a:r></a:p>
+            <a:p><a:r><a:t>List test two:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item two</a:t></a:r></a:p>
+        """,
+    )
+
+    test_mixed_list = mk_test_pptx(
+        "test_mixed_list",
+        """
+            <p>List test one:</p>
+            <ul>
+                <li>Item one</li>
+                <li>Item two</li>
+                <li>Item three:<ol>
+                    <li>Subitem one</li>
+                    <li>Subitem two</li>
+                </ol></li>
+            </ul>
+            <p>List test two:</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+        """,
+        """
+            <a:p><a:r><a:t>List test one:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item two</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item three:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="2"/><a:r><a:t>Subitem one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="2"/><a:r><a:t>Subitem two</a:t></a:r></a:p>
+            <a:p><a:r><a:t>List test two:</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item one</a:t></a:r></a:p>
+            <a:p><a:pPr lvl="1"/><a:r><a:t>Item two</a:t></a:r></a:p>
+        """,
+    )
+
+    test_blockquote = mk_test_pptx(
+        "test_blockquote",
+        """
+        <p>A quote:</p>
+        <blockquote>Alas poor <b>Yorik</b>, I knew him well</blockquote>
+        """,
+        """
+            <a:p><a:r><a:t>A quote:</a:t></a:r></a:p>
+            <a:p>
+                <a:r><a:t>Alas poor </a:t></a:r>
+                <a:r><a:rPr b="1" /><a:t>Yorik</a:t></a:r>
+                <a:r><a:t>, I knew him well</a:t></a:r>
+            </a:p>
+        """,
+    )
+
+    test_pre = mk_test_pptx(
+        "test_pre",
+        """
+        <p>Some code:</p>
+        <pre>int main() {
+    printf("hello world!\\n");
+}</pre>
+        """,
+        """
+            <a:p><a:r><a:t>Some code:</a:t></a:r></a:p>
+            </p:txBody></p:sp>
+            <p:sp>
+                <p:nvSpPr>
+                    <p:cNvPr id="4" name="TextBox 3"/>
+                    <p:cNvSpPr txBox="1"/>
+                    <p:nvPr/>
+                </p:nvSpPr>
+                <p:spPr>
+                    <a:xfrm>
+                        <a:off x="7315200" y="1508760"/>
+                        <a:ext cx="4114800" cy="2743200"/>
+                    </a:xfrm>
+                    <a:prstGeom prst="rect">
+                        <a:avLst/>
+                    </a:prstGeom>
+                    <a:noFill/>
+                </p:spPr>
+                <p:txBody>
+                    <a:bodyPr wrap="none">
+                        <a:spAutoFit/>
+                    </a:bodyPr>
+                    <a:lstStyle/>
+                    <a:p/>
+                    <a:p>
+                        <a:pPr algn="l"/>
+                        <a:r>
+                            <a:rPr sz="1100">
+                                <a:latin typeface="Courier New"/>
+                            </a:rPr>
+                            <a:t>int main() {
+    printf("hello world!\\n");
+}</a:t>
+                        </a:r>
+                    </a:p>
+        """,
+    )
