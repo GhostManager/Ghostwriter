GhostManager/Ghostwriter

commit 0526d0df43e18ae83cb830bdd9170e9044697fae

Rework report export error handling, show error locations
Add `ReportExportError` and wrap user-facing errors in it, annotating
it with the field or location of the error to help users narrow down
template issues.

ghostwriter/modules/reportwriter/base/docx.py
@@ -9,11 +9,10 @@ from docx.opc.exceptions import PackageNotFoundError
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Inches, Pt
-from jinja2.exceptions import TemplateRuntimeError, TemplateSyntaxError, UndefinedError
+from docx.image.exceptions import UnrecognizedImageError
 
 from ghostwriter.commandcenter.models import CompanyInformation, ReportConfiguration
-from ghostwriter.modules.exceptions import InvalidFilterValue
-from ghostwriter.modules.reportwriter.base.base import ExportBase
+from ghostwriter.modules.reportwriter.base.base import ExportBase, ReportExportError
 from ghostwriter.modules.reportwriter.richtext.docx import HtmlToDocxWithEvidence
 
 logger = logging.getLogger(__name__)
@@ -69,12 +68,9 @@ class ExportDocxBase(ExportBase):
         # Create Word document writer using the specified template file
         try:
             self.word_doc = DocxTemplate(template_loc)
-        except PackageNotFoundError:
-            logger.exception(
-                "Failed to load the provided template document because file could not be found: %s",
-                template_loc,
-            )
-            raise
+        except PackageNotFoundError as err:
+            logger.exception("Failed to load the provided template document: %s", template_loc)
+            raise ReportExportError("Template document file could not be found - try re-uploading it") from err
         except Exception:
             logger.exception("Failed to load the provided template document: %s", template_loc)
             raise
@@ -98,7 +94,14 @@ class ExportDocxBase(ExportBase):
     def run(self) -> io.BytesIO:
         self.create_styles()
 
-        self.word_doc.render(self.data, self.jinja_env, autoescape=True)
+        try:
+            ReportExportError.map_jinja2_render_errors(lambda: self.word_doc.render(self.data, self.jinja_env, autoescape=True), "the DOCX template")
+        except UnrecognizedImageError as err:
+            raise ReportExportError(f"Could not load an image: {err}", "the DOCX template") from err
+        except PackageNotFoundError as err:
+            raise ReportExportError("The word template could not be found on the server – try uploading it again.", "the DOCX template") from err
+        except FileNotFoundError as err:
+            raise ReportExportError("An evidence file was missing – try uploading it again.", "the DOCX template") from err
         self.render_properties()
 
         out = io.BytesIO()
@@ -176,20 +179,21 @@ class ExportDocxBase(ExportBase):
             template_src = getattr(self.word_doc.core_properties, attr)
             if not template_src:
                 continue
-            template = self.jinja_env.from_string(template_src)
-            out = template.render(self.data)
+
+            out = ReportExportError.map_jinja2_render_errors(
+                lambda: self.jinja_env.from_string(template_src).render(self.data),
+                f"DOCX property {attr}"
+            )
             setattr(self.word_doc.core_properties, attr, out)
 
-    def process_rich_text_docx(self, text, template_vars, evidences):
+    def process_rich_text_docx(self, name, text, template_vars, evidences):
         """
         Converts HTML from the TinyMCE rich text editor to a Word subdoc.
         """
-        text = self.preprocess_rich_text(text, template_vars)
-        doc = self.word_doc.new_subdoc()
-        try:
-            HtmlToDocxWithEvidence.run(
-                text,
-                doc=doc,
+        ReportExportError.map_jinja2_render_errors(
+            lambda: HtmlToDocxWithEvidence.run(
+                self.preprocess_rich_text(text, template_vars),
+                doc=self.word_doc.new_subdoc(),
                 p_style=self.p_style,
                 evidences=evidences,
                 figure_label=self.label_figure,
@@ -197,14 +201,11 @@ class ExportDocxBase(ExportBase):
                 title_case_captions=self.title_case_captions,
                 title_case_exceptions=self.title_case_exceptions,
                 border_color_width=(self.border_color, self.border_weight) if self.enable_borders else None,
-            )
-        except:
-            # Log input text to help diagnose errors
-            logger.warning("Input text: %r", text)
-            raise
-        return doc
+            ),
+            name
+        )
 
-    def process_extra_fields(self, extra_fields, model, render_rich_text):
+    def process_extra_fields(self, name, extra_fields, model, render_rich_text):
         """
         Process the `extra_fields` dict, filling missing extra fields with empty values and rendering
         rich text
@@ -214,7 +215,10 @@ class ExportDocxBase(ExportBase):
             if field.internal_name not in extra_fields:
                 extra_fields[field.internal_name] = field.empty_value()
             if field.type == "rich_text":
-                extra_fields[field.internal_name] = render_rich_text(str(extra_fields[field.internal_name]))
+                extra_fields[field.internal_name] = render_rich_text(
+                    f"extra field {field.internal_name} of {name}",
+                    str(extra_fields[field.internal_name]),
+                )
 
     @classmethod
     def lint(cls, template_loc: str, p_style: str | None) -> Tuple[List[str], List[str]]:
@@ -255,21 +259,9 @@ class ExportDocxBase(ExportBase):
 
             for var in exporter.jinja_undefined_variables:
                 warnings.append("Undefined variable: {!r}".format(var))
-        except TemplateSyntaxError as error:
-            logger.error("Template syntax error: %s", error)
-            errors.append(f"Template syntax error: {error}")
-        except UndefinedError as error:
-            logger.exception("Template undefined variable error: %s", error)
-            errors.append(f"Template syntax error: {error}")
-        except InvalidFilterValue as error:
-            logger.error("Invalid value provided to filter: %s", error)
-            errors.append(f"Invalid filter value: {error.message}")
-        except TypeError as error:
-            logger.exception("TypeError during template linting")
-            errors.append(f": {error}")
-        except TemplateRuntimeError as error:
-            logger.error("Invalid filter or expression: %s", error)
-            errors.append(f"Invalid filter or expression: {error}")
+        except ReportExportError as error:
+            logger.exception("Template failed linting%s: %s", error.at_error(), error)
+            errors.append(f"Linting failed{error.at_error()}: {error}")
         except Exception:
             logger.exception("Template failed linting")
             errors.append("Template rendering failed unexpectedly")

ghostwriter/modules/reportwriter/project/docx.py
@@ -29,80 +29,82 @@ class ExportProjectDocx(ExportDocxBase, ExportProjectBase):
         """
 
         # Client
-        context["client"]["note_rt"] = base_render(context["client"]["note"])
-        context["client"]["address_rt"] = base_render(context["client"]["address"])
-        process_extra_fields(context["client"]["extra_fields"], Client, base_render)
+        context["client"]["note_rt"] = base_render(f"the note of client {context['client']['name']}", context["client"]["note"])
+        context["client"]["address_rt"] = base_render(f"the address of client {context['client']['name']}", context["client"]["address"])
+        process_extra_fields(f"client {context['client']['name']}", context["client"]["extra_fields"], Client, base_render)
 
         # Assignments
         for assignment in context["team"]:
             if isinstance(assignment, dict):
                 if assignment["note"]:
-                    assignment["note_rt"] = base_render(assignment["note"])
+                    assignment["note_rt"] = base_render(f"the note of person {assignment['name']}", assignment["note"])
 
         # Contacts
         for contact in context["client"]["contacts"]:
             if isinstance(contact, dict):
                 if contact["note"]:
-                    contact["note_rt"] = base_render(contact["note"])
+                    contact["note_rt"] = base_render(f"the note of contact {contact['name']}", contact["note"])
 
         # Objectives
         for objective in context["objectives"]:
             if isinstance(objective, dict):
                 if objective["description"]:
-                    objective["description_rt"] = base_render(objective["description"])
+                    objective["description_rt"] = base_render(f"the description of objective {objective['objective']}", objective["description"])
 
         # Scope Lists
         for scope_list in context["scope"]:
             if isinstance(scope_list, dict):
                 if scope_list["description"]:
-                    scope_list["description_rt"] = base_render(scope_list["description"])
+                    scope_list["description_rt"] = base_render(f"the description of scope {scope_list['name']}", scope_list["description"])
 
         # Targets
         for target in context["targets"]:
             if isinstance(target, dict):
                 if target["note"]:
-                    target["note_rt"] = base_render(target["note"])
+                    target["note_rt"] = base_render(f"the note of target {target['ip_address']}", target["note"])
 
         # Deconfliction Events
         for event in context["deconflictions"]:
             if isinstance(event, dict):
                 if event["description"]:
-                    event["description_rt"] = base_render(event["description"])
+                    event["description_rt"] = base_render(f"the description of deconfliction event {event['title']}", event["description"])
 
         # White Cards
         for card in context["whitecards"]:
             if isinstance(card, dict):
                 if card["description"]:
-                    card["description_rt"] = base_render(card["description"])
+                    card["description_rt"] = base_render(f"the descriptio of whitecard {card['title']}", card["description"])
 
         # Infrastructure
         for asset_type in context["infrastructure"]:
             for asset in context["infrastructure"][asset_type]:
                 if isinstance(asset, dict):
                     if asset["note"]:
-                        asset["note_rt"] = base_render(asset["note"])
+                        asset["note_rt"] = base_render(f"the note of {asset_type} {asset.get('name') or asset['domain']}",
+                                                       asset["note"])
+
         for asset in context["infrastructure"]["domains"]:
-            process_extra_fields(asset["extra_fields"], Domain, base_render)
+            process_extra_fields(f"domain {asset['domain']}", asset["extra_fields"], Domain, base_render)
         for asset in context["infrastructure"]["servers"]:
-            process_extra_fields(asset["extra_fields"], StaticServer, base_render)
+            process_extra_fields(f"server {asset['name']}", asset["extra_fields"], StaticServer, base_render)
 
         # Logs
         for log in context["logs"]:
             for entry in log["entries"]:
-                process_extra_fields(entry["extra_fields"], OplogEntry, base_render)
+                process_extra_fields(f"log entry {entry['description']} of log {log['name']}", entry["extra_fields"], OplogEntry, base_render)
 
     def run(self) -> io.BytesIO:
         context = self.data
         base_context = self.jinja_richtext_base_context()
 
-        def base_render(text):
-            return self.process_rich_text_docx(text, base_context, {})
+        def base_render(name, text):
+            return self.process_rich_text_docx(name, text, base_context, {})
 
         # Fields on Project
         ExportProjectDocx.process_projects_richtext(context, base_render, self.process_extra_fields)
 
         # Project
-        context["project"]["note_rt"] = base_render(context["project"]["note"])
-        self.process_extra_fields(context["project"]["extra_fields"], Project, base_render)
+        context["project"]["note_rt"] = base_render("project.note", context["project"]["note"])
+        self.process_extra_fields(f"project {context['project']['name']}", context["project"]["extra_fields"], Project, base_render)
 
         return super().run()

ghostwriter/modules/reportwriter/report/docx.py
@@ -33,10 +33,10 @@ class ExportReportDocx(ExportDocxBase, ExportReportBase):
         base_context = self.jinja_richtext_base_context()
         base_evidences = {e["friendly_name"]: e for e in context["evidence"]}
 
-        def base_render(text):
-            return self.process_rich_text_docx(text, base_context, base_evidences)
+        def base_render(name, text):
+            return self.process_rich_text_docx(name, text, base_context, base_evidences)
 
-        self.process_extra_fields(context["extra_fields"], Report, base_render)
+        self.process_extra_fields("the report", context["extra_fields"], Report, base_render)
 
         # Findings
         for finding in context["findings"]:
@@ -45,33 +45,33 @@ class ExportReportDocx(ExportDocxBase, ExportReportBase):
             finding_context = self.jinja_richtext_finding_context(base_context, finding)
             finding_evidences = base_evidences | {e["friendly_name"]: e for e in finding["evidence"]}
 
-            def finding_render(text):
-                return self.process_rich_text_docx(text, finding_context, finding_evidences)
+            def finding_render(name, text):
+                return self.process_rich_text_docx(f"{name} of finding {finding['title']}", text, finding_context, finding_evidences)
 
-            self.process_extra_fields(finding["extra_fields"], Finding, finding_render)
+            self.process_extra_fields(f"finding {finding['title']}", finding["extra_fields"], Finding, finding_render)
 
             # Create ``RichText()`` object for a colored severity category
             finding["severity_rt"] = RichText(finding["severity"], color=finding["severity_color"])
             finding["cvss_score_rt"] = RichText(finding["cvss_score"], color=finding["severity_color"])
             finding["cvss_vector_rt"] = RichText(finding["cvss_vector"], color=finding["severity_color"])
             # Create subdocuments for each finding section
-            finding["affected_entities_rt"] = finding_render(finding["affected_entities"])
-            finding["description_rt"] = finding_render(finding["description"])
-            finding["impact_rt"] = finding_render(finding["impact"])
+            finding["affected_entities_rt"] = finding_render("the affected entities section", finding["affected_entities"])
+            finding["description_rt"] = finding_render("the description", finding["description"])
+            finding["impact_rt"] = finding_render("the impact section", finding["impact"])
 
             # Include a copy of ``mitigation`` as ``recommendation`` to match legacy context
-            mitigation_section = finding_render(finding["mitigation"])
+            mitigation_section = finding_render("the mitigation section", finding["mitigation"])
             finding["mitigation_rt"] = mitigation_section
             finding["recommendation_rt"] = mitigation_section
 
-            finding["replication_steps_rt"] = finding_render(finding["replication_steps"])
-            finding["host_detection_techniques_rt"] = finding_render(finding["host_detection_techniques"])
-            finding["network_detection_techniques_rt"] = finding_render(finding["network_detection_techniques"])
-            finding["references_rt"] = finding_render(finding["references"])
+            finding["replication_steps_rt"] = finding_render("the replication steps section", finding["replication_steps"])
+            finding["host_detection_techniques_rt"] = finding_render("the host detection techniques section", finding["host_detection_techniques"])
+            finding["network_detection_techniques_rt"] = finding_render("the network detection techniques section", finding["network_detection_techniques"])
+            finding["references_rt"] = finding_render("the references section", finding["references"])
 
         # Project
-        context["project"]["note_rt"] = base_render(context["project"]["note"])
-        self.process_extra_fields(context["project"]["extra_fields"], Project, base_render)
+        context["project"]["note_rt"] = base_render("the project note", context["project"]["note"])
+        self.process_extra_fields("the project", context["project"]["extra_fields"], Project, base_render)
 
         # Fields on Project
         ExportProjectDocx.process_projects_richtext(context, base_render, self.process_extra_fields)
@@ -79,12 +79,12 @@ class ExportReportDocx(ExportDocxBase, ExportReportBase):
         # Observations
         for observation in context["observations"]:
             if observation["description"]:
-                observation["description_rt"] = base_render(observation["description"])
-            self.process_extra_fields(observation["extra_fields"], Observation, base_render)
+                observation["description_rt"] = base_render(f"the description of observation {observation['title']}", observation["description"])
+            self.process_extra_fields(f"observation {observation['title']}", observation["extra_fields"], Observation, base_render)
 
         # Report Evidence
         # for evidence in context["evidence"]:
-        #    self.process_extra_fields(evidence["extra_fields"], Report, base_render)
+        #    self.process_extra_fields("the evidence", evidence["extra_fields"], Report, base_render)
 
     def run(self) -> io.BytesIO:
         self.process_richtext(self.data)

ghostwriter/modules/reportwriter/base/base.py
@@ -10,6 +10,7 @@ import jinja2
 from django.db.models import Model
 
 from ghostwriter.commandcenter.models import CompanyInformation, ExtraFieldSpec
+from ghostwriter.modules.exceptions import InvalidFilterValue
 from ghostwriter.modules.reportwriter import jinja_funcs, prepare_jinja2_env
 
 
@@ -138,12 +139,15 @@ class ExportBase:
         the jinja data and appending the extension.
         """
 
-        template = self.jinja_env.from_string(filename_template)
         data = self.data.copy()
         data["company_name"] = CompanyInformation.get_solo().company_name
         data["now"] = datetime.now()
 
-        report_name = template.render(data)
+        report_name = ReportExportError.map_jinja2_render_errors(
+            lambda: self.jinja_env.from_string(filename_template).render(data),
+            "the template filename"
+        )
+
         report_name = _replace_filename_chars(report_name)
         if ext is None:
             ext = self.extension()
@@ -176,3 +180,44 @@ def _replace_filename_chars(name):
     """Remove illegal characters from the report name."""
     name = name.replace("–", "-")
     return re.sub(r"[<>:;\"'/\\|?*.,{}\[\]]", "", name)
+
+
+class ReportExportError(Exception):
+    """
+    User-facing error related to report generation
+    """
+    def __init__(self, display_text: str, location: str | None = None):
+        self.display_text = display_text
+        self.location = location
+
+    def __str__(self) -> str:
+        return self.display_text
+
+    def at_error(self) -> str:
+        """
+        If the error has a `location` field, returns a string `" at {the_location}"`, else returns the empty string.
+        """
+        if self.location is None:
+            return ""
+        return f" at {self.location}"
+
+    @classmethod
+    def map_jinja2_render_errors(cls, callback, location: str | None = None):
+        """
+        Runs `callback` with no arguments, catching any Jinja-related exceptions and translating them to `ReportSyntaxError`s
+        while noting the `location`.
+        """
+        try:
+            return callback()
+        except jinja2.TemplateSyntaxError as err:
+            raise ReportExportError(f"Template syntax error: {err}", location) from err
+        except jinja2.UndefinedError as err:
+            raise ReportExportError(f"Template syntax error: {err}", location) from err
+        except InvalidFilterValue as err:
+            raise ReportExportError(f"Invalid filter value: {err.message}", location) from err
+        except jinja2.TemplateError as err:
+            raise ReportExportError(f"Template error: {err}", location) from err
+        except ZeroDivisionError as err:
+            raise ReportExportError("Template attempted to divide by zero", location) from err
+        except TypeError as err:
+            raise ReportExportError(f"Invalid template operation: {err}", location) from err

ghostwriter/modules/reportwriter/base/pptx.py
@@ -8,7 +8,6 @@ from typing import List, Tuple
 from django.conf import settings
 from django.utils.dateformat import format as dateformat
 from bs4 import BeautifulSoup
-from jinja2 import TemplateRuntimeError, TemplateSyntaxError, UndefinedError
 from pptx import Presentation
 from pptx.parts.presentation import PresentationPart
 from pptx.exc import PackageNotFoundError
@@ -17,8 +16,7 @@ from pptx.oxml.ns import nsdecls
 from pptx.enum.text import MSO_AUTO_SIZE
 
 from ghostwriter.commandcenter.models import CompanyInformation
-from ghostwriter.modules.exceptions import InvalidFilterValue
-from ghostwriter.modules.reportwriter.base.base import ExportBase
+from ghostwriter.modules.reportwriter.base.base import ExportBase, ReportExportError
 from ghostwriter.modules.reportwriter.richtext.pptx import HtmlToPptxWithEvidence
 
 logger = logging.getLogger(__name__)
@@ -58,18 +56,8 @@ class ExportBasePptx(ExportBase):
 
         try:
             self.ppt_presentation = Presentation(template_loc)
-        except ValueError:
-            logger.exception(
-                "Failed to load the provided template document because it is not a PowerPoint file: %s",
-                template_loc,
-            )
-            raise
-        except PackageNotFoundError:
-            logger.exception(
-                "Failed to load the provided template document because file could not be found: %s",
-                template_loc,
-            )
-            raise
+        except PackageNotFoundError as err:
+            raise ReportExportError("Template document file could not be found - try re-uploading it") from err
         except Exception:
             logger.exception(
                 "Failed to load the provided template document for unknown reason: %s",
@@ -79,22 +67,19 @@ class ExportBasePptx(ExportBase):
 
         self.company_config = CompanyInformation.get_solo()
 
-    def process_rich_text_pptx(self, text, slide, shape, template_vars, evidences):
+    def process_rich_text_pptx(self, name, text, slide, shape, template_vars, evidences):
         """
         Converts HTML from the TinyMCE rich text editor and inserts it into the passed in slide and shape
         """
-        text = self.preprocess_rich_text(text, template_vars)
-        try:
-            HtmlToPptxWithEvidence.run(
-                text,
+        ReportExportError.map_jinja2_render_errors(
+            lambda: HtmlToPptxWithEvidence.run(
+                self.preprocess_rich_text(text, template_vars),
                 slide=slide,
                 shape=shape,
                 evidences=evidences,
-            )
-        except:
-            # Log input text to help diagnose errors
-            logger.warning("Input text: %r", text)
-            raise
+            ),
+            name
+        )
 
     def process_footers(self):
         """
@@ -157,21 +142,9 @@ class ExportBasePptx(ExportBase):
                 warnings.append(
                     "Template can be used, but it has slides when it should be empty (see documentation)"
                 )
-        except TemplateSyntaxError as error:
-            logger.error("Template syntax error: %s", error)
-            errors.append(f"Template syntax error: {error}")
-        except UndefinedError as error:
-            logger.error("Template undefined variable error: %s", error)
-            errors.append(f"Template syntax error: {error}")
-        except InvalidFilterValue as error:
-            logger.error("Invalid value provided to filter: %s", error)
-            errors.append(f"Invalid filter value: {error.message}")
-        except TypeError as error:
-            logger.exception("TypeError during template linting")
-            errors.append(f": {error}")
-        except TemplateRuntimeError as error:
-            logger.error("Invalid filter or expression: %s", error)
-            errors.append(f"Invalid filter or expression: {error}")
+        except ReportExportError as error:
+            logger.exception("Template failed linting%s: %s", error.at_error(), error)
+            errors.append(f"Linting failed{error.at_error()}: {error}")
         except Exception:
             logger.exception("Template failed linting")
             errors.append("Template rendering failed unexpectedly")

ghostwriter/modules/reportwriter/base/xlsx.py
@@ -3,7 +3,7 @@ import io
 
 from xlsxwriter.workbook import Workbook
 
-from ghostwriter.modules.reportwriter.base.base import ExportBase
+from ghostwriter.modules.reportwriter.base.base import ExportBase, ReportExportError
 from ghostwriter.modules.reportwriter.richtext.plain_text import html_to_plain_text
 
 
@@ -34,13 +34,15 @@ class ExportXlsxBase(ExportBase):
     def extension(cls) -> str:
         return "xlsx"
 
-    def process_rich_text_xlsx(self, html, template_vars, evidences) -> str:
+    def process_rich_text_xlsx(self, name, html, template_vars, evidences) -> str:
         """
         Converts HTML from the TinyMCE rich text editor and returns a plain string
         for use in XLSX cells
         """
-        text = self.preprocess_rich_text(html, template_vars)
-        return html_to_plain_text(text, evidences)
+        return ReportExportError.map_jinja2_render_errors(
+            lambda: html_to_plain_text(self.preprocess_rich_text(html, template_vars), evidences),
+            name,
+        )
 
     def run(self) -> io.BytesIO:
         self.workbook.close()

ghostwriter/modules/reportwriter/project/pptx.py
@@ -91,6 +91,7 @@ class ProjectSlidesMixin:
 
         finding_body_shape = shapes.placeholders[1]
         self.process_rich_text_pptx(
+            "project.note",
             self.data["project"]["note"],
             slide=slide,
             shape=finding_body_shape,

ghostwriter/modules/reportwriter/report/pptx.py
@@ -95,6 +95,7 @@ class ExportReportPptx(ExportBasePptx, ExportReportBase, ProjectSlidesMixin):
             if observation.get("description", "").strip():
                 observation_context = self.jinja_richtext_base_context()
                 self.process_rich_text_pptx(
+                    f"the description of observation {observation['title']}",
                     observation["description"],
                     slide=observation_slide,
                     shape=observation_body_shape,
@@ -192,6 +193,7 @@ class ExportReportPptx(ExportBasePptx, ExportReportBase, ProjectSlidesMixin):
                 finding_context = self.jinja_richtext_finding_context(base_context, finding)
                 finding_evidences = base_evidences | {e["friendly_name"]: e for e in finding["evidence"]}
                 self.process_rich_text_pptx(
+                    f"the description of finding `{finding['title']}`",
                     finding["description"],
                     slide=finding_slide,
                     shape=finding_body_shape,

commit 5a4c383947187f0d515ff794ecd51ec18c29addb

Fix docx extra fields
0526d0df43e18ae83cb830bdd9170e9044697fae accidentally broke it by not
returning the subdocument from `process_rich_text_docx`

ghostwriter/modules/reportwriter/base/docx.py
@@ -190,10 +190,11 @@ class ExportDocxBase(ExportBase):
         """
         Converts HTML from the TinyMCE rich text editor to a Word subdoc.
         """
+        doc = self.word_doc.new_subdoc()
         ReportExportError.map_jinja2_render_errors(
             lambda: HtmlToDocxWithEvidence.run(
                 self.preprocess_rich_text(text, template_vars),
-                doc=self.word_doc.new_subdoc(),
+                doc=doc,
                 p_style=self.p_style,
                 evidences=evidences,
                 figure_label=self.label_figure,
@@ -204,6 +205,7 @@ class ExportDocxBase(ExportBase):
             ),
             name
         )
+        return doc
 
     def process_extra_fields(self, name, extra_fields, model, render_rich_text):
         """