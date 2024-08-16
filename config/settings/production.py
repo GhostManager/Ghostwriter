0de6dd049c0c5278a8c38df19f07d8ff0ef22496
Christopher Maddalena <chris.maddalena@protonmail.com>
2024-08-16 15:26:06 -0700

    Changed `EMAIL_BACKEND` default to console from Mailgun example

--- config/settings/production.py
+++ config/settings/production.py
@@ -85,7 +85,7 @@ ADMIN_URL = env("DJANGO_ADMIN_URL")
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
 INSTALLED_APPS += ["anymail"]  # noqa F405
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 ANYMAIL = {
     "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
