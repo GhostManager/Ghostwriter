fc0e130d018c847c13302edee51166db70858cff
Christopher Maddalena <chris.maddalena@protonmail.com>
2024-09-05 10:44:53 -0700

    Added some validation for report template filetypes

--- ghostwriter/reporting/validators.py
+++ ghostwriter/reporting/validators.py
@@ -5,6 +5,11 @@ from django.core.validators import FileExtensionValidator
 
 EVIDENCE_ALLOWED_EXTENSIONS = ["txt", "md", "log", "jpg", "jpeg", "png"]
 
+DOCX_ALLOWED_EXTENSIONS = ["docx", "doc", "docm", "dotx", "dotm"]
+PPTX_ALLOWED_EXTENSIONS = ["pptx", "ppt", "pptm", "potx", "potm", "ppsx", "ppsm"]
+TEMPLATE_ALLOWED_EXTENSIONS = DOCX_ALLOWED_EXTENSIONS + PPTX_ALLOWED_EXTENSIONS
+
+
 def validate_evidence_extension(value):
     """
     Enforce a limited allowlist for filetypes. Allowed filetypes are limited to
