e6d36a94623bab88529e199c70216a866890b2dd
Christopher Maddalena <chris.maddalena@protonmail.com>
2024-09-10 11:50:04 -0700

    Updated for delayed release

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ import environ
 
 __version__ = "4.3.0"
 VERSION = __version__
-RELEASE_DATE = "5 September 2024"
+RELEASE_DATE = "10 September 2024"
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = ROOT_DIR / "ghostwriter"
