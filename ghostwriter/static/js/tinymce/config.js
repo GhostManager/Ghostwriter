a778cc993dff655aaded7b128939bc815df566d7
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-02-07 16:07:28 -0800

    Changed `tinymceInit` to target modal text areas only

--- ghostwriter/static/js/tinymce/config.js
+++ ghostwriter/static/js/tinymce/config.js
@@ -358,8 +358,10 @@
 
 })($ || django.jQuery);
 
-function tinymceInit() {
-    tinymce.init(GW_TINYMCE_BASIC_CONFIG);
+function tinymceLogInit() {
+    let logConfig = { ...GW_TINYMCE_BASIC_CONFIG };
+    logConfig.selector = '.modal-content textarea:not(.empty-form textarea, .empty-form, .no-auto-tinymce)';
+    tinymce.init(logConfig);
 }
 
 function tinymceRemove() {
