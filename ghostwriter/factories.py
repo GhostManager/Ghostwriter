25ebec425079378bf3b5aedba8e6ed5bd195fb3e
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-07-02 14:05:45 -0700

    Added `require_2fa` to user factory

--- ghostwriter/factories.py
+++ ghostwriter/factories.py
@@ -54,6 +54,7 @@ class UserFactory(factory.django.DjangoModelFactory):
     enable_observation_create = False
     enable_observation_edit = False
     enable_observation_delete = False
+    require_2fa = False
 
     @factory.post_generation
     def groups(self, create, extracted, **kwargs):
