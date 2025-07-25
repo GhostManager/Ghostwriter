d3bfa2b1e7fc384934d3f194bfa475fecb02232c
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-07-25 14:13:23 -0700

    Changed env name to `POSTGRES_CONN_MAX_AGE` and set default to `0`

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,7 @@ STATICFILES_DIRS += [
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("POSTGRES_CONN_MAX_AGE", default=0)  # noqa F405
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -28,7 +28,7 @@ CACHES = {
         "LOCATION": env("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # Mimicing memcache behavior.
+            # Mimicking memcache behavior.
             # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
             "IGNORE_EXCEPTIONS": True,
         },
