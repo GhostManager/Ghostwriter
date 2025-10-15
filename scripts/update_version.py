087d45a53d174fe0f703c4ef3f5144d932edc41f
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-10-15 16:14:06 -0700

    Initial commit of version update workflow

---
+++ scripts/update_version.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+version = sys.argv[1].lstrip('v')
+release_date = sys.argv[2]
+
+# Update VERSION file
+with open('VERSION', 'w') as f:
+    f.write(f"v{version}\n{release_date}\n")
+
+# Update base.py
+path = '../config/settings/base.py'
+with open(path, 'r') as f:
+    content = f.read()
+
+content = re.sub(r'__version__\s*=\s*["\'].*?["\']', f'__version__ = "{version}"', content)
+content = re.sub(r'RELEASE_DATE\s*=\s*["\'].*?["\']', f'RELEASE_DATE = "{release_date}"', content)
+
+with open(path, 'w') as f:
+    f.write(content)
