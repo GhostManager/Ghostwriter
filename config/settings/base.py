57c4aba8042d7c69445bdf8d6df05cca3dd5ecee
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-10-15 18:36:19 -0700

    Moved comm it into same step for variables

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,7 @@ from django.contrib.messages import constants as messages
 # 3rd Party Libraries
 import environ
 
-__version__ = "6.0.5"
+__version__ = "6.0.4"
 VERSION = __version__
 RELEASE_DATE = "16 October 2025"
 
