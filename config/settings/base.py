151d78e0751e5505904dba739289d34e2a34e70c
Christopher Maddalena <chris.maddalena@protonmail.com>
2026-03-03 09:25:30 -0800

    Updated dates

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ import environ
 
 __version__ = "6.2.5"
 VERSION = __version__
-RELEASE_DATE = "2 March 2026"
+RELEASE_DATE = "3 March 2026"
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = ROOT_DIR / "ghostwriter"
