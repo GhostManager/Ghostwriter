2d34c6d7eae63cfb43f3c6a79e9df9e3b5ace0c5
Christopher Maddalena <chris.maddalena@protonmail.com>
2026-05-01 17:51:35 -0700

    Fixed error that came from non-string values in rich text custom fields

--- ghostwriter/commandcenter/templatetags/extra_fields.py
+++ ghostwriter/commandcenter/templatetags/extra_fields.py
@@ -3,15 +3,30 @@ import json
 
 # Django Imports
 from django import template
+from django.utils.encoding import force_str
 
 from ghostwriter.commandcenter.models import ExtraFieldSpec
 
 register = template.Library()
 
 
+def _coerce_rich_text_value(value):
+    if value is None:
+        return ""
+    if isinstance(value, str):
+        return value
+    try:
+        return json.dumps(value, indent="\t")
+    except (TypeError, ValueError):
+        return force_str(value)
+
+
 @register.filter
 def get_extra_field(extra_fields: dict, spec: ExtraFieldSpec):
-    return spec.value_of(extra_fields)
+    value = spec.value_of(extra_fields)
+    if spec.type == "rich_text":
+        return _coerce_rich_text_value(value)
+    return value
 
 
 @register.filter
