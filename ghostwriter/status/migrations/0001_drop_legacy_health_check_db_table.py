db09c1288b45df79caebee6c1ddb4ad2cbdc33a7
Christopher Maddalena <chris.maddalena@protonmail.com>
2026-06-08 14:12:11 -0700

    Migration now only drops the legacy health-check table
    The migration now only drops the legacy health-check table and no longer modifies django_migrations.

--- ghostwriter/status/migrations/0001_drop_legacy_health_check_db_table.py
+++ ghostwriter/status/migrations/0001_drop_legacy_health_check_db_table.py
@@ -4,10 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
-            sql=[
-                "DROP TABLE IF EXISTS health_check_db_testmodel;",
-                "DELETE FROM django_migrations WHERE app = 'db';",
-            ],
+            sql="DROP TABLE IF EXISTS health_check_db_testmodel;",
             reverse_sql=migrations.RunSQL.noop,
         ),
     ]
