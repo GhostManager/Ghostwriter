3dc4f0eb40b40241a94939416dd3977718b45bb3
Christopher Maddalena <chris.maddalena@protonmail.com>
2026-07-10 13:08:22 -0700

    Added test case for dict values

--- ghostwriter/shepherd/tests/test_templatetags.py
+++ ghostwriter/shepherd/tests/test_templatetags.py
@@ -8,6 +8,18 @@ from ghostwriter.shepherd.templatetags.shepherd_tags import category_value
 
 
 class CategoryValueTests(SimpleTestCase):
+    def test_dict_values_render_keys_and_nonempty_nested_values(self):
+        value = {
+            "source": "business",
+            "categories": ["technology", "security"],
+            "empty": None,
+        }
+
+        self.assertEqual(
+            category_value(value),
+            "source: business, categories: technology, security",
+        )
+
     def test_set_values_are_rendered_in_deterministic_order(self):
         self.assertEqual(
             category_value({"technology", "business"}),
